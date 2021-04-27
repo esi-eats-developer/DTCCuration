3b97e13e66f830ab5b2a28fad8a770b9b6e32c3d
esi-eats-developer <82066675+esi-eats-developer@users.noreply.github.com>
2021-04-27 10:47:23 +0900

    update

--- js/card.js
+++ js/card.js
@@ -6,9 +6,7 @@ var card = {
         "text" : "빠르면 빠를수록 좋다! 마스크 착용으로 인한 피부 손상, 점점 다가오는 피부 노화. 지금부터 관리해요!",
         "nutrient" : "콜라겐, 비타민C",
         "img" : "",
-        "special" : "",
-        "link" : "https://www.qualqual.co.kr/q/2xFmlwzWA",
-        "link-total" : "https://www.qualqual.co.kr/q/6Ebw45VSZ"
+        "special" : ""
     },
     // <!-- A2M 대상 카드 -->
     "A2M" : {
@@ -17,8 +15,7 @@ var card = {
         "nutrient" : "",
         "img" : "",
         "special" : "",
-        "link" : "",
-        "link-total" : "https://www.qualqual.co.kr/q/6EaR1Jo0W"
+        "link" : ""
     },
     // <!-- A2F 대상 카드 -->
     "A2F" : {
@@ -26,17 +23,13 @@ var card = {
         "text" : "갱년기 골밀도 감소로 인해 폐경 이후 여성의 94%가 골다공증을 앓는다고 해요 :( 시큰거리는 손/발목 건강기능식품으로 보호해요!",
         "nutrient" : "칼슘, 마그네슘",
         "img" : "",
-        "special" : "",
-        "link" : "https://www.qualqual.co.kr/q/2xF3g4Xiy",
-        "link-total" : "https://www.qualqual.co.kr/q/6Ebd9Q1Gf"
+        "special" : ""
     },
     "A3MF" : {
         "title" : "뼈/관절",
         "text" : "날이 갈수록 시리고 뻐근한 몸. 관절 및 연골의 필수 구성성분도 섭취를 통해 간단히 챙길 수 있어요!",
         "nutrient" : "MSM",
         "img" : "",
-        "special" : "",
-        "link" : "https://www.qualqual.co.kr/q/6EbOQmS1u",
-        "link-total" : "https://www.qualqual.co.kr/q/6EarlRF1H"
+        "special" : ""
     }
 }
