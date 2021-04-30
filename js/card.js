280f8471b164c30d572b44baa608165188cf9206
esi-eats-developer <82066675+esi-eats-developer@users.noreply.github.com>
2021-04-30 11:17:44 +0900

    update!@

--- js/card.js
+++ js/card.js
@@ -5,7 +5,7 @@ var card = {
         "title" : "피부 관리",
         "text" : "빠르면 빠를수록 좋다! 마스크 착용으로 인한 피부 손상, 점점 다가오는 피부 노화. 지금부터 관리해요!",
         "nutrient" : "콜라겐, 비타민C",
-        "img" : "",
+        "img" : "콜라겐.PNG",
         "special" : ""
         // "link" : "https://www.qualqual.co.kr/q/2xFmlwzWA"
     },
@@ -23,7 +23,7 @@ var card = {
         "title" : "여성 건강(갱년기)",
         "text" : "갱년기 골밀도 감소로 인해 폐경 이후 여성의 94%가 골다공증을 앓는다고 해요 :( 시큰거리는 손/발목 건강기능식품으로 보호해요!",
         "nutrient" : "칼슘, 마그네슘",
-        "img" : "",
+        "img" : "칼슘.PNG",
         "special" : ""
         // "link" : "https://www.qualqual.co.kr/q/2xF3g4Xiy"
     },
@@ -31,7 +31,7 @@ var card = {
         "title" : "뼈/관절",
         "text" : "날이 갈수록 시리고 뻐근한 몸. 관절 및 연골의 필수 구성성분도 섭취를 통해 간단히 챙길 수 있어요!",
         "nutrient" : "칼슘, MSM",
-        "img" : "",
+        "img" : "칼슘2.PNG",
         "special" : ""
         // "link" : "https://www.qualqual.co.kr/q/6EbOQmS1u"
     }
